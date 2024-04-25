37abb443cddb8020468270653a578e5b29cbf986
shimUnBo <pencer118@gmail.com>
2024-04-26 04:25:19 +0900

    ws,gpt 수정

--- src/main/java/com/goorm/goormIDE/api/config/WebConfig.java
+++ src/main/java/com/goorm/goormIDE/api/config/WebConfig.java
@@ -11,10 +11,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(false);
+                .allowedOrigins("*");
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
     }
 
     @Override
--- src/main/java/com/goorm/goormIDE/api/controller/settingKey/GetSetting.java
+++ src/main/java/com/goorm/goormIDE/api/controller/settingKey/GetSetting.java
@@ -1,28 +1,36 @@
 package com.goorm.goormIDE.api.controller.settingKey;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 public class GetSetting {
 
-    @Value("${chat.url:}")
+    @Value("${chat.url:ws://localhost:8080/ws/chat}")
     private String webSocketUrl;
-    @Value("${gpt.key:}")
+    @Value("${gpt.key:aaaa}")
     private String chatGptKey;
 
     @PostMapping("/get/wsUrl")
-    public String chatSet() {
-//        System.out.println("WebSocket URL passed: " + webSocketUrl);
-        return webSocketUrl;
+    public ResponseEntity<Map<String, String>> chatSet() {
+        Map<String, String> response = new HashMap<>();
+        System.out.println(webSocketUrl+">>>>>>>>>>");
+        response.put("url", webSocketUrl);
+        return ResponseEntity.ok(response);
     }
     @PostMapping("/get/gptKey")
-    public String gptKey() {
-//        System.out.println("gptKey@@@ : " + chatGptKey);
-        return chatGptKey;
+    public ResponseEntity<Map<String, String>> gptKey() {
+        Map<String, String> response = new HashMap<>();
+        response.put("key", chatGptKey);
+        System.out.println("gptKey@@@ : " + chatGptKey);
+        return ResponseEntity.ok(response);
     }
 }
 
